c6e58e526b5ea2d28c4894e80005f93d806a9d5c
Wasley Abraao <willcomeo022@gmail.com>
2020-12-21 21:38:58 -0300

    Initial Complete

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+    <script type="text/javascript" src="charting_library/charting_library.standalone.js"></script>
+    <script type="text/javascript" src="datafeeds/udf/dist/polyfills.js"></script>
+    <script type="text/javascript" src="datafeeds/udf/dist/bundle.js"></script>
+	<!-- STYLES -->
+
+	<style {csp-style-nonce}>
+		* {
+			transition: background-color 300ms ease, color 300ms ease;
+		}
+		*:focus {
+			background-color: rgba(221, 72, 20, .2);
+			outline: none;
+		}
+		html, body {
+			color: rgba(33, 37, 41, 1);
+			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+			font-size: 16px;
+			margin: 0;
+			padding: 0;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+			text-rendering: optimizeLegibility;
+		}
+		header {
+			background-color: rgba(247, 248, 249, 1);
+			padding: .4rem 0 0;
+		}
+		.menu {
+			padding: .4rem 2rem;
+		}
+		header ul {
+			border-bottom: 1px solid rgba(242, 242, 242, 1);
+			list-style-type: none;
+			margin: 0;
+			overflow: hidden;
+			padding: 0;
+			text-align: right;
+		}
+		header li {
+			display: inline-block;
+		}
+		header li a {
+			border-radius: 5px;
+			color: rgba(0, 0, 0, .5);
+			display: block;
+			height: 44px;
+			text-decoration: none;
+		}
+		header li.menu-item a {
+			border-radius: 5px;
+			margin: 5px 0;
+			height: 38px;
+			line-height: 36px;
+			padding: .4rem .65rem;
+			text-align: center;
+		}
+		header li.menu-item a:hover,
+		header li.menu-item a:focus {
+			background-color: rgba(221, 72, 20, .2);
+			color: rgba(221, 72, 20, 1);
+		}
+		header .logo {
+			float: left;
+			height: 44px;
+			padding: .4rem .5rem;
+		}
+		header .menu-toggle {
+			display: none;
+			float: right;
+			font-size: 2rem;
+			font-weight: bold;
+		}
+		header .menu-toggle button {
+			background-color: rgba(221, 72, 20, .6);
+			border: none;
+			border-radius: 3px;
+			color: rgba(255, 255, 255, 1);
+			cursor: pointer;
+			font: inherit;
+			font-size: 1.3rem;
+			height: 36px;
+			padding: 0;
+			margin: 11px 0;
+			overflow: visible;
+			width: 40px;
+		}
+		header .menu-toggle button:hover,
+		header .menu-toggle button:focus {
+			background-color: rgba(221, 72, 20, .8);
+			color: rgba(255, 255, 255, .8);
+		}
+		header .heroe {
+			margin: 0 auto;
+			max-width: 1100px;
+			padding: 1rem 1.75rem 1.75rem 1.75rem;
+		}
+		header .heroe h1 {
+			font-size: 2.5rem;
+			font-weight: 500;
+		}
+		header .heroe h2 {
+			font-size: 1.5rem;
+			font-weight: 300;
+		}
+		section {
+			margin: 0 auto;
+			max-width: 1100px;
+			padding: 2.5rem 1.75rem 3.5rem 1.75rem;
+		}
+		section h1 {
+			margin-bottom: 2.5rem;
+		}
+		section h2 {
+			font-size: 120%;
+			line-height: 2.5rem;
+			padding-top: 1.5rem;
+		}
+		section pre {
+			background-color: rgba(247, 248, 249, 1);
+			border: 1px solid rgba(242, 242, 242, 1);
+			display: block;
+			font-size: .9rem;
+			margin: 2rem 0;
+			padding: 1rem 1.5rem;
+			white-space: pre-wrap;
+			word-break: break-all;
+		}
+		section code {
+			display: block;
+		}
+		section a {
+			color: rgba(221, 72, 20, 1);
+		}
+		section svg {
+			margin-bottom: -5px;
+			margin-right: 5px;
+			width: 25px;
+		}
+		.further {
+			background-color: rgba(247, 248, 249, 1);
+			border-bottom: 1px solid rgba(242, 242, 242, 1);
+			border-top: 1px solid rgba(242, 242, 242, 1);
+		}
+		.further h2:first-of-type {
+			padding-top: 0;
+		}
+		footer {
+			background-color: rgba(221, 72, 20, .8);
+			text-align: center;
+		}
+		footer .environment {
+			color: rgba(255, 255, 255, 1);
+			padding: 2rem 1.75rem;
+		}
+		footer .copyrights {
+			background-color: rgba(62, 62, 62, 1);
+			color: rgba(200, 200, 200, 1);
+			padding: .25rem 1.75rem;
+		}
+		@media (max-width: 559px) {
+			header ul {
+				padding: 0;
+			}
+			header .menu-toggle {
+				padding: 0 1rem;
+			}
+			header .menu-item {
+				background-color: rgba(244, 245, 246, 1);
+				border-top: 1px solid rgba(242, 242, 242, 1);
+				margin: 0 15px;
+				width: calc(100% - 30px);
+			}
+			header .menu-toggle {
+				display: block;
+			}
+			header .hidden {
+				display: none;
+			}
+			header li.menu-item a {
+				background-color: rgba(221, 72, 20, .1);
+			}
+			header li.menu-item a:hover,
+			header li.menu-item a:focus {
+				background-color: rgba(221, 72, 20, .7);
+				color: rgba(255, 255, 255, .8);
+			}
+		}
+	</style>
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<div id="tv_chart_container"></div>
+
+<!-- SCRIPTS -->
+
+<script type="text/javascript">
+
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
+    function initOnReady() {
+        var widget = window.tvWidget = new TradingView.widget({
+            // debug: true, // uncomment this line to see Library errors and warnings in the console
+            fullscreen: true,
+            symbol: 'AAPL',
+            interval: '1D',
+            container_id: "tv_chart_container",
+
+            //	BEWARE: no trailing slash is expected in feed URL
+            datafeed: new Datafeeds.UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
+            library_path: "charting_library/",
+            locale: getParameterByName('lang') || "en",
+
+            disabled_features: ["use_localstorage_for_settings"],
+            enabled_features: ["study_templates"],
+            charts_storage_url: 'https://saveload.tradingview.com',
+            charts_storage_api_version: "1.1",
+            client_id: 'tradingview.com',
+            user_id: 'public_user_id',
+            theme: getParameterByName('theme'),
+        });
+    };
+
+    window.addEventListener('DOMContentLoaded', initOnReady, false);
+</script>
+
+
+<!-- TradingView Widget BEGIN -->
+<!--<div class="tradingview-widget-container">-->
+<!--    <div id="tradingview_0d72d"></div>-->
+<!--    <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT Chart</span></a> by TradingView</div>-->
+<!--    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>-->
+<!--    <script type="text/javascript">-->
+<!--        new TradingView.widget(-->
+<!--            {-->
+<!--                "width": 980,-->
+<!--                "height": 610,-->
+<!--                "symbol": "BINANCE:BTCUSDT",-->
+<!--                "interval": "1",-->
+<!--                "timezone": "Etc/UTC",-->
+<!--                "theme": "light",-->
+<!--                "style": "1",-->
+<!--                "locale": "en",-->
+<!--                "toolbar_bg": "#f1f3f6",-->
+<!--                "enable_publishing": false,-->
+<!--                "allow_symbol_change": true,-->
+<!--                "container_id": "tradingview_0d72d"-->
+<!--            }-->
+<!--        );-->
+<!--    </script>-->
+<!--</div>-->
+<!-- TradingView Widget END -->
+
+
+<!-- -->
+
+</body>
+</html>
