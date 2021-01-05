e7310a485cffb6969886c4cf661fa93da2125c41
Wasley Abraao <willcomeo022@gmail.com>
2021-01-05 14:10:27 -0300

    Add FXCM Feed Data

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -214,25 +214,26 @@
 
     function initOnReady() {
         var widget = window.tvWidget = new TradingView.widget({
-            // debug: true, // uncomment this line to see Library errors and warnings in the console
-            fullscreen: true,
-            symbol: 'AAPL',
-            interval: '1D',
-            container_id: "tv_chart_container",
+			// debug: true, // uncomment this line to see Library errors and warnings in the console
+			fullscreen: true,
+			symbol: 'EUR/USD',
+			interval: '1D',
+			container_id: "tv_chart_container",
 
-            //	BEWARE: no trailing slash is expected in feed URL
-            datafeed: new Datafeeds.UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-            library_path: "charting_library/",
-            locale: getParameterByName('lang') || "en",
+			//	BEWARE: no trailing slash is expected in feed URL
+			// datafeed: new Datafeeds.UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
+			datafeed: new Datafeeds.UDFCompatibleDatafeed("http://3.8.140.107", 10000),
+			library_path: "charting_library/",
+			locale: getParameterByName('lang') || "en",
 
-            disabled_features: ["use_localstorage_for_settings"],
-            enabled_features: ["study_templates"],
-            charts_storage_url: 'https://saveload.tradingview.com',
-            charts_storage_api_version: "1.1",
-            client_id: 'tradingview.com',
-            user_id: 'public_user_id',
-            theme: getParameterByName('theme'),
-        });
+			disabled_features: ["use_localstorage_for_settings"],
+			enabled_features: ["study_templates"],
+			charts_storage_url: 'https://saveload.tradingview.com',
+			charts_storage_api_version: "1.1",
+			client_id: 'infinity-tradingview.com',
+			user_id: 'public_user_id',
+			theme: getParameterByName('theme'),
+		}); 
     };
 
     window.addEventListener('DOMContentLoaded', initOnReady, false);
